6150da50b863877a2ab0915150dbe125ebbc359a
Khash <khashayar.m.003@gmail.com>
2023-11-05 00:32:14 +0330

    fixing cart

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,13 @@
 import {Fragment} from 'react';
-
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg'
-
-
 import classes from './Header.module.css'
 
 const Header = () => {
   return <Fragment>
     <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
         <img src={mealsImage} alt='table of food' />
