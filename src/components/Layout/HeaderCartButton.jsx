3f62528aed17d143dd11380b08a34facff98b5b9
Khash <khashayar.m.003@gmail.com>
2023-12-13 17:37:16 +0330

    animation added to cart icon whenever item is added or removed

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect , useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
@@ -6,16 +6,39 @@ import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
 
-  const cartCtx =  useContext(CartContext);
+  const [btnIsHighlighted,setBtnIsHighlighted] = useState(false)
 
+  const cartCtx =  useContext(CartContext);
+  const { items } = cartCtx;
   // reduce allows us to transform an array of data into a single value.
   //first value a function which will be called for you , second value is a starting value.
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+ 
+  //animation
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0 ){
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300);
+
+    //cleanup function for timer
+    // returning a function in useeffect is automatically a cleanup function by React
+    return () => {
+      clearImmediate(timer);
+    }
+
+  }, [items])
 
 
-  return <button className={classes.button} onClick={props.onClick}>
+  return <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
         <CartIcon />
     </span>
